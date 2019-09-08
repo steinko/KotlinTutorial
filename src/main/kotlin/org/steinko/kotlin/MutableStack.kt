072ffd93bdc53eb2b526b98b7d50b3ea155afa84
steinkorsveien <steinkorsveien@10.0.0.83>
2019-09-08 10:27:32 +0200

    Added size

--- src/main/kotlin/org/steinko/kotlin/MutableStack.kt
+++ src/main/kotlin/org/steinko/kotlin/MutableStack.kt
@@ -3,7 +3,9 @@ package org.steinko.kotlin
 class MutableStack<E> (vararg items: E) {  
    private val elements = items.toMutableList()
    
+   fun push(element:E) = elements.add(element)
    fun isEmpty() = elements.isEmpty()
+   fun size()  = elements.size
 
 
 }
--- src/test/kotlin/org/steinko/kotlin/MutableStackTest.kt
+++ src/test/kotlin/org/steinko/kotlin/MutableStackTest.kt
@@ -6,17 +6,45 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class MutableStackTest: Spek({
 
     Feature ("Testing of MutableStack"){
+        var classUnderTest:  MutableStack<Int?>  =  MutableStack<Int?>() 
+       
        Scenario ("Should be empty") {
     
-           val classUnderTest: MutableStack<Int>  = MutableStack<Int>()
+         
                
           Then("should be empty"){
-               assertTrue(classUnderTest.isEmpty())             
+               classUnderTest = MutableStack<Int?>()
+               var isEmpty = classUnderTest.isEmpty()
+               assertTrue(isEmpty)             
            }
+          
+          Then("should be size 0") {   
+               assertEquals(classUnderTest.size(), 0)
+          }
+      }
+      Scenario("Should add a elemenet") { 
+         
+          When ("Add Item") {  
+          
+             classUnderTest.push(1)
+          }
+          
+          Then("should not not be empty") { 
+               assertFalse(classUnderTest.isEmpty())
+           }
+           
+           Then("should contain a element") { 
+               assertEquals(classUnderTest.size(),1)
+           }
+           
+           
+           
       }
     }
 })
